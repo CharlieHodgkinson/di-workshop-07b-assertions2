ee6738caec2fee804a84c869bb463b6beda5eec7
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 11:50:47 +0000

    exercise 9

--- test/exercise08.test.js
+++ test/exercise08.test.js
@@ -1,26 +1,26 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-    suite('Unit Tests Exercise 8 - Number Comparison 1',function(){
-      // 8 - .isAbove() => a > b , .isAtMost() => a <= b 
-      // the tests will fail by default
-      // use .isAbove() and isAtMost() to make the following tests pass
-      test('Comparing the length of a string', function(){
-        assert.isAtMost('hello'.length , 5);
-      });
-      test('Comparing two numbers', function(){
-         assert.isAbove(1, 0);
-      });
-      test('Comparing special numbers', function(){
-         assert.isAbove(Math.PI, 3);
-      });
-      test('Comparing random numbers', function(){
-         assert.isAtMost(1 - Math.random(), 1);
-      });
+//     suite('Unit Tests Exercise 8 - Number Comparison 1',function(){
+//       // 8 - .isAbove() => a > b , .isAtMost() => a <= b 
+//       // the tests will fail by default
+//       // use .isAbove() and isAtMost() to make the following tests pass
+//       test('Comparing the length of a string', function(){
+//         assert.isAtMost('hello'.length , 5);
+//       });
+//       test('Comparing two numbers', function(){
+//          assert.isAbove(1, 0);
+//       });
+//       test('Comparing special numbers', function(){
+//          assert.isAbove(Math.PI, 3);
+//       });
+//       test('Comparing random numbers', function(){
+//          assert.isAtMost(1 - Math.random(), 1);
+//       });
       
-    })
+//     })
 
-// number comparison allows for you to determine a range 
-// in which certain variables or properties should be found
-// one test, using randomness, will still produce a number lower
-// than one, but randomness introduces indeterminancy into our tests.
+// // number comparison allows for you to determine a range 
+// // in which certain variables or properties should be found
+// // one test, using randomness, will still produce a number lower
+// // than one, but randomness introduces indeterminancy into our tests.
