81a9230286d669ff085da9bdab784ebe51b43c71
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 12:13:55 +0000

    exercise 12

--- test/exercise11.test.js
+++ test/exercise11.test.js
@@ -1,12 +1,12 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-    suite('Unit Tests Exercise 11 - Array Identitiy',function(){
-      // 11 - #isArray vs #isNotArray 
-      test('does split return an array?', function() {
-        assert.isArray('isThisAnArray?'.split(''), 'String.prototype.split() returns an Array');
-      })
-      test('does indexOf return an array?', function() {  
-        assert.isNotArray([1,2,3].indexOf(2), 'indexOf returns a number.');
-      });
-    })
+//     suite('Unit Tests Exercise 11 - Array Identitiy',function(){
+//       // 11 - #isArray vs #isNotArray 
+//       test('does split return an array?', function() {
+//         assert.isArray('isThisAnArray?'.split(''), 'String.prototype.split() returns an Array');
+//       })
+//       test('does indexOf return an array?', function() {  
+//         assert.isNotArray([1,2,3].indexOf(2), 'indexOf returns a number.');
+//       });
+//     })
