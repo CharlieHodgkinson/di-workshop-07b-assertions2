1c80455f7343808fdff4aa63d9a1f33e3d5a2610
Charlotte Hodgkinson <charlotte.hodgkinson@rs-components.com>
2020-01-13 12:11:58 +0000

    exercise 11

--- test/exercise10.test.js
+++ test/exercise10.test.js
@@ -1,19 +1,19 @@
-var chai = require('chai');
-var assert = chai.assert;
+// var chai = require('chai');
+// var assert = chai.assert;
 
-function weirdNumbers(delta) {
-  return (1 + delta - Math.random());
-}
-    suite('Unit Tests Exercise 10 - Approximation',function(){
-      // 10 - .approximately 
-      // .approximately(actual, expected, range, [message])
-      // actual = expected +/- range
-      // Choose the minimum range (3rd parameter) to make the test always pass
-      // it should be less than 1
-      test('Approximation part 1', function(){
-        assert.approximately(weirdNumbers(0.5) , 1, 0.5);
-      });
-      test('Approximation part 2', function(){
-        assert.approximately(weirdNumbers(0.2) , 1, 0.2);
-      });
-    })
+// function weirdNumbers(delta) {
+//   return (1 + delta - Math.random());
+// }
+//     suite('Unit Tests Exercise 10 - Approximation',function(){
+//       // 10 - .approximately 
+//       // .approximately(actual, expected, range, [message])
+//       // actual = expected +/- range
+//       // Choose the minimum range (3rd parameter) to make the test always pass
+//       // it should be less than 1
+//       test('Approximation part 1', function(){
+//         assert.approximately(weirdNumbers(0.5) , 1, 0.5);
+//       });
+//       test('Approximation part 2', function(){
+//         assert.approximately(weirdNumbers(0.2) , 1, 0.2);
+//       });
+//     })
